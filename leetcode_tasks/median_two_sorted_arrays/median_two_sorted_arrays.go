75cd2fb677d6a1f466de94caab9abe90f960fed7
Vladislav Abrashkin <isneezh@gmail.com>
2022-05-22 12:17:09 +0300

    median two sorted arrays

--- leetcode_tasks/median_two_sorted_arrays/median_two_sorted_arrays.go
+++ leetcode_tasks/median_two_sorted_arrays/median_two_sorted_arrays.go
@@ -1,5 +1,35 @@
 package main
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	return 0.0
+	n := len(nums1)
+	m := len(nums2)
+
+	if n+m == 0 {
+		return 0.0
+	}
+
+	mergedArr := make([]int, 0, m+n)
+	mergedArr = append(mergedArr, nums1...)
+	mergedArr = append(mergedArr, make([]int, m)...)
+	p1, p2 := n-1, m-1
+
+	for p := m + n - 1; p >= 0; p-- {
+		if p2 < 0 {
+			break
+		}
+
+		if p1 >= 0 && mergedArr[p1] > nums2[p2] {
+			mergedArr[p] = mergedArr[p1]
+			p1--
+		} else {
+			mergedArr[p] = nums2[p2]
+			p2--
+		}
+	}
+
+	if (n+m)%2 != 0 {
+		return float64(mergedArr[(n+m)/2])
+	} else {
+		return float64((float64(mergedArr[(n+m)/2-1] + mergedArr[(n+m)/2])) / 2)
+	}
 }
--- leetcode_tasks/median_two_sorted_arrays/median_two_sorted_arrays_test.go
+++ leetcode_tasks/median_two_sorted_arrays/median_two_sorted_arrays_test.go
@@ -1,7 +1,25 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-func TestFindMedianSortedArrays(t testing.T) {
+	"github.com/stretchr/testify/assert"
+)
 
+func TestFindMedianSortedArrays(t *testing.T) {
+	var tests = []struct {
+		name     string
+		expected float64
+		nums1    []int
+		nums2    []int
+	}{
+		{"even count", 3.5, []int{1, 2, 3}, []int{4, 5, 6}},
+		{"odd count", 4, []int{1, 2, 3}, []int{4, 5, 6, 7}},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, findMedianSortedArrays(tt.nums1, tt.nums2))
+		})
+	}
 }
